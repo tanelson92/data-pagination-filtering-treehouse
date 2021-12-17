20b4e37fe9fdab9195cd1e6b5050de792620be9c
tanelson92 <trent@pegasuscart.com>
2021-12-17 10:39:57 -0600

    Format project.

--- js/script.js
+++ js/script.js
@@ -15,19 +15,19 @@ This function will create a new element based on requirements listed.
 */
 
 function newElement(element, classes) {
-   //Build a new custom element, and add classes if provided. 
-   let newItem = document.createElement(element);
-   let moreThanOne = Array.isArray(classes);
-   if (classes) {
-      if (moreThanOne) {
-         for (let i = 0; i < classes.length; i++) {
-            newItem.classList.add(classes[i]);
-         }
-      } else {
-         newItem.className = classes;
+  //Build a new custom element, and add classes if provided.
+  let newItem = document.createElement(element);
+  let moreThanOne = Array.isArray(classes);
+  if (classes) {
+    if (moreThanOne) {
+      for (let i = 0; i < classes.length; i++) {
+        newItem.classList.add(classes[i]);
       }
-   }
-   return newItem;
+    } else {
+      newItem.className = classes;
+    }
+  }
+  return newItem;
 }
 
 /* 
@@ -36,21 +36,22 @@ function newElement(element, classes) {
 */
 
 function performSearch(search) {
-   let students = data;
-   let filtered = [];
-   //Filter students based on user search query.
-   for (const student of students) {
-      const studentName = `${student.name.first} ${student.name.last}`.toLowerCase();
-      const searchTerm = search.toLowerCase();
-      const visible = studentName.includes(searchTerm);
-      if (visible) {
-         filtered.push(student);
-      }
-   }
+  let students = data;
+  let filtered = [];
+  //Filter students based on user search query.
+  for (const student of students) {
+    const studentName =
+      `${student.name.first} ${student.name.last}`.toLowerCase();
+    const searchTerm = search.toLowerCase();
+    const visible = studentName.includes(searchTerm);
+    if (visible) {
+      filtered.push(student);
+    }
+  }
 
-   //pass filtered list of students to showPage, and update pagination.
-   showPage(filtered);
-   addPagination(filtered);
+  //pass filtered list of students to showPage, and update pagination.
+  showPage(filtered);
+  addPagination(filtered);
 }
 
 /* 
@@ -59,34 +60,34 @@ function performSearch(search) {
 */
 
 function buildSearch() {
-   //create new search bar including: input, button, and search button image. 
-   const header = document.querySelector('.header');
-   const search = newElement('div', 'student-search');
-   const input = newElement('input');
-   input.type = 'text';
-   input.placeholder = 'Search by name...'
-   const button = newElement('button');
-   const buttonImg = newElement('img');
-   buttonImg.src = 'img/icn-search.svg';
-   button.append(buttonImg);
-   search.append(input, button);
+  //create new search bar including: input, button, and search button image.
+  const header = document.querySelector(".header");
+  const search = newElement("div", "student-search");
+  const input = newElement("input");
+  input.type = "text";
+  input.placeholder = "Search by name...";
+  const button = newElement("button");
+  const buttonImg = newElement("img");
+  buttonImg.src = "img/icn-search.svg";
+  button.append(buttonImg);
+  search.append(input, button);
 
-   //add search bar to the header. 
-   header.insertAdjacentHTML('beforeend', search.outerHTML);
+  //add search bar to the header.
+  header.insertAdjacentHTML("beforeend", search.outerHTML);
 
-   //Set an event listener for searches submitted on the search bar by click or pressing the "enter" key.
-   const searchInp = document.querySelector('.student-search');
-   const searchBtn = searchInp.querySelector('button img');
-   searchBtn.addEventListener('click', (e) => {
-      const query = document.querySelector('.student-search input').value;
+  //Set an event listener for searches submitted on the search bar by click or pressing the "enter" key.
+  const searchInp = document.querySelector(".student-search");
+  const searchBtn = searchInp.querySelector("button img");
+  searchBtn.addEventListener("click", (e) => {
+    const query = document.querySelector(".student-search input").value;
+    performSearch(query);
+  });
+  searchInp.addEventListener("keypress", (e) => {
+    if (e.keyCode === 13) {
+      const query = document.querySelector(".student-search input").value;
       performSearch(query);
-   });
-   searchInp.addEventListener('keypress', (e) => {
-      if (e.keyCode === 13) {
-         const query = document.querySelector('.student-search input').value;
-         performSearch(query);
-      }
-   });
+    }
+  });
 }
 
 /*
@@ -95,32 +96,35 @@ function buildSearch() {
 */
 
 function buildStudent(student) {
-   //create student listing
-   let studentList = document.querySelector('.student-list');
-   let listItem = newElement('li', [ 'student-item', 'cf' ]);
-   listItem.setAttribute('data-name', `${student.name.first} ${student.name.last}`);
-      
-      //add student picture, name, and email to listing.
-      let details = newElement('div', 'student-details');
-      let image = newElement('img', 'avatar');
-      image.src = student.picture.thumbnail;
-      image.alt = 'Profile Picture';
-      let name = newElement('h3');
-      name.textContent = `${student.name.first} ${student.name.last}`;
-      let email = newElement('span', 'email');
-      email.textContent = student.email;
-      details.append(image, name, email);
+  //create student listing
+  let studentList = document.querySelector(".student-list");
+  let listItem = newElement("li", ["student-item", "cf"]);
+  listItem.setAttribute(
+    "data-name",
+    `${student.name.first} ${student.name.last}`
+  );
 
-      //add the date student joined, to listing.
-      let joined = newElement('div', 'joined-details');
-         let joinDate = newElement('span');
-         joinDate.textContent = `Joined ${student.registered.date}`;
-      joined.append(joinDate);
+  //add student picture, name, and email to listing.
+  let details = newElement("div", "student-details");
+  let image = newElement("img", "avatar");
+  image.src = student.picture.thumbnail;
+  image.alt = "Profile Picture";
+  let name = newElement("h3");
+  name.textContent = `${student.name.first} ${student.name.last}`;
+  let email = newElement("span", "email");
+  email.textContent = student.email;
+  details.append(image, name, email);
 
-   //add the student listing to our overall list. 
-   listItem.append(details, joined);
-   //add students to the page.
-   studentList.append(listItem);
+  //add the date student joined, to listing.
+  let joined = newElement("div", "joined-details");
+  let joinDate = newElement("span", "date");
+  joinDate.textContent = `Joined ${student.registered.date}`;
+  joined.append(joinDate);
+
+  //add the student listing to our overall list.
+  listItem.append(details, joined);
+  //add students to the page.
+  studentList.append(listItem);
 }
 
 /*
@@ -128,14 +132,14 @@ function buildStudent(student) {
    Show no results page and provide a "Go Back" button.
 */
 function showNoResults() {
-   let studentList = document.querySelector('.student-list');
-   let noResults = `<div class='noResults'><span class='noResultsText'>Sorry no results matched your search!</span><span class='btnGoBack'>Go Back</span></div>`;
-   studentList.innerHTML = noResults;
+  let studentList = document.querySelector(".student-list");
+  let noResults = `<div class='noResults'><span class='noResultsText'>Sorry no results matched your search!</span><span class='btnGoBack'>Go Back</span></div>`;
+  studentList.innerHTML = noResults;
 
-   const btn = document.querySelector('.btnGoBack');
-   btn.addEventListener('click',(e) => {
-      showPage(data, 1);
-   });
+  const btn = document.querySelector(".btnGoBack");
+  btn.addEventListener("click", (e) => {
+    showPage(data, 1);
+  });
 }
 
 /*
@@ -144,26 +148,29 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 function showPage(list, page = 1) {
-   //set max students visible per page. 
-   let perPage = 9;
-   let min = (page * perPage) - perPage;
-   let max = page * perPage;
+  //set max students visible per page.
+  let perPage = 9;
+  let min = page * perPage - perPage;
+  let max = page * perPage;
 
-   // remove previous student listings from page.
-   let studentList = document.querySelector('.student-list');
-   studentList.innerHTML = "";
+  // remove previous student listings from page.
+  let studentList = document.querySelector(".student-list");
+  studentList.innerHTML = "";
 
-   // If no results are returned show notice and "go back" button.
-   if (list.length <= 0) { showNoResults(); }
+  // If no results are returned show notice and "go back" button.
+  if (list.length <= 0) {
+    showNoResults();
+  }
 
-   //For each student build a profile listing. 
-   for (let i = min; i >= min && i < max; i++) {
-      let student = list[i];
-      if (typeof student === 'undefined') { return; }
-      buildStudent(student);
-   }
+  //For each student build a profile listing.
+  for (let i = min; i >= min && i < max; i++) {
+    let student = list[i];
+    if (typeof student === "undefined") {
+      return;
+    }
+    buildStudent(student);
+  }
 }
-
 
 /*
 Create the `addPagination` function
@@ -171,43 +178,43 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination(list) {
-   //calculate pages available. 
-   let totalPages = Math.ceil(list.length / 9);
-   
-   //remove any old pagination buttons before creating new ones. 
-   let linkList = document.querySelector('.link-list');
-   linkList.innerHTML = ""
-   
-   //for each page in totalpages, add a page button. 
-   for (let i = 1; i <= totalPages; i++) {
-      let buttonContainer = newElement('li');
-      let button = newElement('button');
-      button.type = 'button';
-      if (i === 1) { button.className = 'active' }
-      button.textContent = i;
-      buttonContainer.append(button);
+  //calculate pages available.
+  let totalPages = Math.ceil(list.length / 9);
 
-      //add button to page. 
-      linkList.append(buttonContainer);
-   }
- 
-   //Set an event listener for changing a page.
-   linkList.addEventListener('click', (e) => {
-      if (e.target.tagName === "BUTTON") {
-         //change page when user clicks on a page button. 
-         const buttons = document.querySelectorAll('.link-list li button');
-         const newActive = e.target;
-         showPage(data, newActive.textContent);
-         //remove previously active button class, and add class to active page. 
-         for (const button of buttons) {
-            button.className = '';
-         }
-         newActive.className = 'active';
+  //remove any old pagination buttons before creating new ones.
+  let linkList = document.querySelector(".link-list");
+  linkList.innerHTML = "";
+
+  //for each page in totalpages, add a page button.
+  for (let i = 1; i <= totalPages; i++) {
+    let buttonContainer = newElement("li");
+    let button = newElement("button");
+    button.type = "button";
+    if (i === 1) {
+      button.className = "active";
+    }
+    button.textContent = i;
+    buttonContainer.append(button);
+
+    //add button to page.
+    linkList.append(buttonContainer);
+  }
+
+  //Set an event listener for changing a page.
+  linkList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      //change page when user clicks on a page button.
+      const buttons = document.querySelectorAll(".link-list li button");
+      const newActive = e.target;
+      showPage(data, newActive.textContent);
+      //remove previously active button class, and add class to active page.
+      for (const button of buttons) {
+        button.className = "";
       }
-   });
+      newActive.className = "active";
+    }
+  });
 }
-
-
 
 // Call functions
 buildSearch();
